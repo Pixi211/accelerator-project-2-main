7c735146c0cf32125ecc0ee565c13a2adcc8fdaa
Semenets Ekaterina <cortes.lex@gmail.com>
2025-12-08 15:47:25 +0300

    fix: Добавляет облака

--- source/js/initialize-form.js
+++ source/js/initialize-form.js
@@ -1,18 +1,3 @@
-// export function initializeForm() {
-//   const inputs = document.querySelectorAll('.subscription-form__input');
-
-//   inputs.forEach((input) => {
-//     input.addEventListener('focus', function() {
-//       this.placeholder = this.getAttribute('data-placeholder');
-//     });
-
-//     input.addEventListener('blur', function() {
-//       if (!this.value) {
-//         this.placeholder = this.id === 'phone' ? 'Телефон' : 'E-mail';
-//       }
-//     });
-//   });
-// }
 export function initializeForm() {
   const inputs = document.querySelectorAll('.subscription-form__input');
   inputs.forEach((input) => {
